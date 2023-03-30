8428f63cc275575cfc7ef82a4a2c25e86ed97bb7
svantesodergard <svante03+github@gmail.com>
2023-03-30 11:31:15 +0200

    Added error messages when filling in order form

--- js/scripts.js
+++ js/scripts.js
@@ -116,49 +116,58 @@ function orderPage(id) {
 
 document.getElementById("submit").addEventListener("click", save);
 
-function validate(){
+document.getElementById("order-css").addEventListener("input", validate);
 
+function validate(){
     if (document.getElementById("name").value.length<2 ||
     document.getElementById("name").value.length>50) {
-        console.log("namn fel");
-        document.getElementById("name").value="fel";
+        document.getElementById("error-name").style.display = "block";
         return false;
+    } else {
+        document.getElementById("error-name").style.display = "none";
     } 
 
     const mailPattern = /@/;
 
     if (!mailPattern.test(document.getElementById("e-mail").value)||
     document.getElementById("e-mail").value.length>50) {
-        console.log("mail fel");
+        document.getElementById("error-e-mail").style.display = "block";
         return false;
-    }
+    } else {
+        document.getElementById("error-e-mail").style.display = "none";
+    } 
 
     const pattern = /^([+]46)\s*(7[0236])\s*(\d{4})\s*(\d{3})$/;
 
     if (!pattern.test(document.getElementById("phone").value)){
-        console.log("nummer fel");
-        document.getElementById("phone").value="fel";
-        return false
-    }
+        document.getElementById("error-phone").style.display = "block";
+        return false;
+    } else {
+        document.getElementById("error-phone").style.display = "none";
+    } 
 
-    if(document.getElementById("address").value.length<4||
-    document.getElementById("address").value.length>50) {
-        console.log("adress fel");
-        return false; 
-    }
+    if (document.getElementById("address").value.length>50) {
+        document.getElementById("error-address").style.display = "block";
+        return false;
+    } else {
+        document.getElementById("error-address").style.display = "none";
+    } 
 
     const zipPattern = /^\d{3}\s\d{2}$/;
 
     if(!zipPattern.test(document.getElementById("zip-code").value)) {
-        console.log("zip fel")
+        document.getElementById("error-zip-code").style.display = "block";
         return false;
-    }
+    } else {
+        document.getElementById("error-zip-code").style.display = "none";
+    } 
 
-    if(document.getElementById("city").value.length<4||
-    document.getElementById("city").value.length>50) {
-        console.log("city fel");
-        return false; 
-    }
+    if (document.getElementById("city").value.length>50) {
+        document.getElementById("error-city").style.display = "block";
+        return false;
+    } else {
+        document.getElementById("error-city").style.display = "none";
+    } 
 
 console.log("rätt");
 
